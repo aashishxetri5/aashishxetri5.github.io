640c7b3fbbc55270495b07a393f84246cdc83a85
Aashish Katwal <aashiskutwal5@gmail.com>
2022-02-10 14:36:30 +0545

    About page created

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -1,1 +1,22 @@
 // All JS here.
+
+$(document).ready(function () {
+    $(".center-nav nav-item a").click(function () {
+        $(this).css("color", "white !important");
+    });
+
+
+  $(window).scroll(function () {
+    if (this.scrollY >= 25) {
+      $(".nav-h").addClass("nav-header");
+    } else {
+      $(".nav-h").removeClass("nav-header");
+    }
+  });
+
+  $(".ham-menu").click(function () {
+    $(this).toggleClass("active");
+    $(".fa-bars").toggleClass("fa-times");
+    $(".navbar-center").toggleClass("active");
+  });
+});
